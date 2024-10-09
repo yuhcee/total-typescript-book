25b455116ae6d9e045da6c44a9f3ef27e5b68c95
yuhcee <egbouchenna001@gmail.com>
2024-10-10 00:03:58 +0100

    :sparkles: complete chapter 2 exercises

--- src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
+++ src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
@@ -1,19 +1,22 @@
 type MyObj = {
-  foo: string;
-  bar: number;
-  baz: boolean;
+    foo: string;
+    bar: number;
+    baz: boolean;
 };
 
 const acceptsObj = (obj: MyObj) => {};
 
 acceptsObj({
-  // Autocomplete in here!
+    // Autocomplete in here!
+    foo: 'Uchenna',
+    bar: 12,
+    baz: true,
 });
 
 document.addEventListener(
-  // Autocomplete this string!
-  "",
-  (event) => {
-    console.log(event);
-  },
+    // Autocomplete this string!
+    '',
+    (event) => {
+        console.log(event);
+    }
 );
--- src/016.5-ide-superpowers/050-refactor.problem.ts
+++ src/016.5-ide-superpowers/050-refactor.problem.ts
@@ -1,5 +1,10 @@
 const func = () => {
-  // Refactor this to be its own function
-  const randomPercentage = `${(Math.random() * 100).toFixed(2)}%`;
-  console.log(randomPercentage);
+    // Refactor this to be its own function
+    const randomPercentage = getRandomPercentage();
+
+    console.log(randomPercentage);
 };
+
+function getRandomPercentage() {
+    return `${(Math.random() * 100).toFixed(2)}%`;
+}
